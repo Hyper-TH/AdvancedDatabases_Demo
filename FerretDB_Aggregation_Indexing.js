13b00084b1766e4978c6afe66511aa145a256e6c
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2025-11-07 18:43:35 +0000

    Added indexing

--- FerretDB_Aggregation_Indexing.js
+++ FerretDB_Aggregation_Indexing.js
@@ -15,3 +15,26 @@ db.dishes.aggregate([
     { $sort: { totalCalories: -1 } }
 ]);
 
+// Indexing example
+// Single field indexes
+db.dishes.createIndex({ Protein: 1 }); // Ascending index on Protein field
+
+// Compound indexes
+// Useful for queries that require more protein with less calories
+db.dishes.createIndex({ Protein: 1, Calories: -1 }); // Ascending on Protein, Descending on Calories
+
+// Useful for queries that sort by Cost and then by DName
+db.dishes.createIndex({ Cost: 1, DName: 1 }); // Ascending on both Cost and DName
+
+// Unique index
+// (DName must be unique for this to work)
+db.dishes.createIndex({ DName: 1 }, { unique: true }); 
+
+// Get list of indexes
+db.dishes.getIndexes();
+
+// Drop index
+db.dishes.dropIndex({ Protein: 1 });
+
+// Drop all indexes
+db.dishes.dropIndexes('*');
